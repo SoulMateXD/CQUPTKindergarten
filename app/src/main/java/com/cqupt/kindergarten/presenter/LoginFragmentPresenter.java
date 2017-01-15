e22e38d3be3663f9144e862c90f5eaddea2cb5d4
zhanghanxuan <476426037@qq.com>
2017-01-15 11:38:38 +0800

    两个接口

--- app/src/main/java/com/cqupt/kindergarten/presenter/LoginFragmentPresenter.java
+++ app/src/main/java/com/cqupt/kindergarten/presenter/LoginFragmentPresenter.java
@@ -3,7 +3,6 @@ package com.cqupt.kindergarten.presenter;
 import com.cqupt.kindergarten.base.BasePresenter;
 import com.cqupt.kindergarten.model.DataManager;
 import com.cqupt.kindergarten.ui.ui_interface.ILoginFragmentInterface;
-import com.cqupt.kindergarten.ui.ui_interface.IMineFragmentInterface;
 
 /**
  * Created by inferno on 2017/1/14.
---
+++ app/src/main/java/com/cqupt/kindergarten/ui/ui_interface/IHomeFragmentInterface.java
@@ -0,0 +1,10 @@
+package com.cqupt.kindergarten.ui.ui_interface;
+
+import com.cqupt.kindergarten.base.MvpView;
+
+/**
+ * Created by zhx on 2017/1/15.
+ */
+
+public interface IHomeFragmentInterface extends MvpView{
+}
